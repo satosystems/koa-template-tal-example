a367e62756086fdaf149e8c27781b5fbbd2e91c1
Satoshi Ogata <satosystems@gmail.com>
2023-09-10 16:15:58 +0900

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import Koa from 'koa';
+import fs from 'fs';
+import tal  from 'template-tal';
 
 const app = new Koa();
 
 app.use(async (ctx: Koa.Context) => {
-  ctx.body = 'Hello World';
+  const html = fs.readFileSync('./src/index.html', 'utf8');
+  const data = {
+    foo: "FOO",
+    bar: () => "BAR",
+    baz: () => new Promise(resolve => setTimeout(() => resolve('BAZ'), 5000)),
+    getBasket: () => ['apple', 'orange', 'banana']
+  };
+  tal.process(html, data).then((result: string) => {
+    ctx.body = result;
+  });
 });
 
 app.listen(3000);
